aef5ab748cfa5e8aa2fae02556c7535aa215f81c
ForrestWeston <forrest@protocol.ai>
2017-12-20 17:25:24 -0800

    gx: update go-log
    License: MIT
    Signed-off-by: ForrestWeston <forrest@protocol.ai>

--- importer/chunk/splitting.go
+++ importer/chunk/splitting.go
@@ -4,7 +4,7 @@ package chunk
 import (
 	"io"
 
-	logging "gx/ipfs/QmSpJByNKFX1sCsHBEp3R73FL4NF6FnQTEGyNAXHm2GS52/go-log"
+	logging "gx/ipfs/QmeMFK13EDShD6L5dvrE9TEiuqtqRMBfxpgNkGxUYY4Vk5/go-log"
 )
 
 var log = logging.Logger("chunk")
